28c9685cd2be3e33fb13b90b60f5655536c4fdd1
gubressan <gustavobressanin6@gmail.com>
2026-04-10 09:26:59 -0300

    fix: add bcrypt/node-pre-gyp native modules to webpack externals

--- backend-api/webpack.config.js
+++ backend-api/webpack.config.js
@@ -1,8 +1,16 @@
 module.exports = function (options) {
   return {
     ...options,
-    externals: {
-      sharp: 'commonjs sharp',
-    },
+    externals: [
+      // Preservar externals existentes do NestJS
+      ...(Array.isArray(options.externals) ? options.externals : options.externals ? [options.externals] : []),
+      // Modulos nativos C++ que nao podem ser bundlados
+      function ({ request }, callback) {
+        if (/^(sharp|bcrypt|@mapbox\/node-pre-gyp|mock-aws-s3|aws-sdk|nock)$/.test(request)) {
+          return callback(null, 'commonjs ' + request);
+        }
+        callback();
+      },
+    ],
   };
 };
